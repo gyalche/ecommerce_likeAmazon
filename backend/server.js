63c313ee757479a28c2713fde843c9d5cbdc0b1c
Dawa <sherpadawa01@outlook.com>
2022-07-13 22:54:39 +0545

    working on backend

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import data from './data.js';
+import cors from 'cors';
+const app = express();
+app.use(cors());
+app.get('/api/products', function (req, res) {
+  res.send(data.products);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, function () {
+  console.log(`server at http://localhost:${port}`);
+});
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
-
+// import data from '../data';
+import axios from '../axios';
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const ans = await axios.get('/api/products');
+        setProducts(ans.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Featured Products</h1>
 
       <div className="products">
-        {data.products.map((product) => (
+        {products?.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
